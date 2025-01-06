e5d5d822efcc13893ec2ddae92d1e4c730338f80
Sudhi-Varun <sudhivarun1988@gmail.com>
2025-01-06 23:54:19 +0530

    Added java support

---
+++ js/java.js
@@ -0,0 +1,83 @@
+let editor;
+
+document.addEventListener('DOMContentLoaded', async function() {
+    prepareEditor();
+    resizeBoxes();
+});
+
+function showSpinner() {
+    document.getElementById('spinnerContainer').style.display = 'flex';
+}
+
+function hideSpinner() {
+    document.getElementById('spinnerContainer').style.display = 'none';
+}
+
+function prepareEditor() {
+    editor = ace.edit("editor");
+    editor.setTheme("ace/theme/eclipse");
+    editor.session.setMode("ace/mode/java");
+    editor.resize(true);
+    editor.setOptions({
+        fontSize: "15px",
+        highlightActiveLine: false,
+        printMargin: false,
+        showGutter: false
+    });
+    editor.setValue('', 1);
+    editor.container.style.lineHeight = 1.5;
+    editor.insert(`public class Main {
+    public static void main(String[] args) {
+        
+    }
+}`);
+}
+
+function resizeBoxes() {
+    const windowHeight = window.innerHeight - 80;
+    const windowWidth = window.innerWidth;
+    const titleBar = document.querySelector('.title-bar-ide');
+    const titleBarHeight = titleBar ? titleBar.offsetHeight : 0; 
+                
+    const boxHeight = windowHeight - titleBarHeight;
+    document.querySelector('.should-resize-this').style.cssText = `height: ${boxHeight}px; width: ${windowWidth}px;`;
+    document.querySelector('.container').style.cssText = 'margin-left: 0px; margin-right: 0px;';
+    document.querySelector('.right-box').style.cssText = `height: ${boxHeight}px; overflow-y: auto;`;
+
+    var totalHeight = [...document.querySelectorAll('.right-buttons-holder, .prg-input')]
+    .reduce((sum, div) => {
+        const style = getComputedStyle(div);
+        const margin = parseFloat(style.marginTop) + parseFloat(style.marginBottom);
+        const height = div.offsetHeight + margin;
+        return sum + height;
+        }, 0);
+    totalHeight += titleBarHeight;
+
+    document.querySelector('.prg-editor').style.height = windowHeight - totalHeight -8 + 'px';
+}
+
+document.querySelector('.clear-the-code').addEventListener('click', function() {
+    document.querySelector('.prg-output').innerHTML = '';
+    editor.setOption("showGutter", false);
+    editor.setValue('', 1);
+    editor.insert(`public class Main {
+    public static void main(String[] args) {
+        
+    }
+}`);
+    document.querySelector('.program-input').value = '';
+});
+
+document.querySelector('.reload-the-code').addEventListener('click', async function() {
+    location.reload();
+});
+
+function smoothScrollToBottom(className) {
+    const scrollableDiv = document.querySelector(`.${className}`);
+    if (scrollableDiv) {
+        scrollableDiv.scroll({
+            top: scrollableDiv.scrollHeight,
+            behavior: 'smooth'
+        });
+    }
+}
---
+++ js/javacompiler.js
@@ -0,0 +1,118 @@
+document.querySelector('.run-the-code').addEventListener('click', function () {
+    showSpinner();
+    editor.setOption("showGutter", false);
+    document.querySelector('.prg-output').innerHTML = `<pre class='console-output-here' style='padding-bottom:2px;padding-left:5px;margin-bottom:0px'>Output:\n</pre>`;
+    var fs = BrowserFS.BFSRequire("fs");
+    var process = BrowserFS.BFSRequire('process');
+    var buffer = BrowserFS.BFSRequire("buffer").Buffer;
+
+    (function () {
+        var mfs = new BrowserFS.FileSystem.MountableFileSystem();
+        BrowserFS.initialize(mfs);
+        mfs.mount('/tmp', new BrowserFS.FileSystem.InMemory());
+        var home = new BrowserFS.FileSystem.InMemory();
+        mfs.mount('/home', home);
+
+        function mountZipAndLoad(n) {
+            mfs.mount("/doppio_home", new BrowserFS.FileSystem.ZipFS(new buffer(n)));
+            if (document.readyState === 'complete') {
+                afterLoad();
+            } else {
+                window.addEventListener('load', afterLoad);
+            }
+        }
+
+        var t = new XMLHttpRequest();
+        t.open("GET", "/cdn/doppio/doppio_home.zip");
+        t.responseType = "arraybuffer";
+        t.addEventListener("load", function () {
+            mountZipAndLoad(t.response);
+        });
+        t.send();
+    })();
+
+    function afterLoad() {
+        var textareaOutput = document.querySelector('.console-output-here');
+        var textareaCode = editor.getValue();
+        var textareaInput = document.querySelector('.program-input');
+        
+        process.initializeTTYs();
+        process.chdir('/home');
+        
+        process.stdout.removeAllListeners('data');
+        process.stderr.removeAllListeners('data');
+
+        process.stdout.on('data', function (data) {
+            textareaOutput.textContent += data.toString();
+            hideSpinner();
+            editor.setOption("showGutter", false);
+            window.scrollTo({
+                top: document.body.scrollHeight,
+                behavior: 'smooth'
+            });
+            smoothScrollToBottom('right-box');
+        });
+        
+        process.stderr.on('data', function (data) {
+            textareaOutput.textContent += data.toString()
+            if (data.toString().includes("Error:"))
+            {
+                hideSpinner();
+                editor.setOption("showGutter", true);
+            }
+            window.scrollTo({
+                top: document.body.scrollHeight,
+                behavior: 'smooth'
+            });
+            smoothScrollToBottom('right-box');
+        });
+        
+
+        fs.readdir('/tmp', function (err, files) {
+            if (!err) {
+                files.forEach(file => fs.unlinkSync(`/tmp/${file}`));
+            }
+            fs.writeFileSync('/tmp/Main.java', textareaCode);
+
+            Doppio.VM.CLI(
+                ['-classpath', '/doppio_home', 'classes.util.Javac', '/tmp/Main.java'],
+                {
+                    doppioHomePath: '/doppio_home'
+                },
+                function (exitCode) {
+                    if (exitCode === 0) {
+                        Doppio.VM.CLI(
+                            ['-classpath', '/tmp', 'Main'],
+                            {
+                                doppioHomePath: '/doppio_home'
+                            }
+                        );
+
+                        if (textareaInput.value != null && textareaInput.value !== "")
+                            process.stdin.write(textareaInput.value + "\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n");
+                        else
+                            process.stdin.write("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n");
+                            
+                        hideSpinner();
+                        window.scrollTo({
+                            top: document.body.scrollHeight,
+                            behavior: 'smooth'
+                        });
+                        smoothScrollToBottom('right-box');
+
+                    } else {
+                        textareaOutput.textContent += "Error: Compilation failed.\n";
+                        
+                        hideSpinner();
+                        editor.setOption("showGutter", true);
+                        window.scrollTo({
+                            top: document.body.scrollHeight,
+                            behavior: 'smooth'
+                        });
+                        smoothScrollToBottom('right-box');
+                    }
+                }
+            );
+        });
+    }
+});
